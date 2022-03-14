ab3ead6cac190bc99ed982a545d878602d0cf8a8
Robert Butler <robertbutler@protonmail.com>
2022-03-14 11:25:22 +0000

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+}
+`;
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
